45c3ee72a4ef9280d6ece111fe7b077dd97fcd9e
Aman Kaushik <hankwilliamson49@gmail.com>
2020-04-26 15:14:15 +0530

    v1.3.0

--- Stop-Watch-App/app/src/main/java/com/example/stopwatchapp/MainActivity.java
+++ Stop-Watch-App/app/src/main/java/com/example/stopwatchapp/MainActivity.java
@@ -1,26 +1,100 @@
 package com.example.stopwatchapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class MainActivity extends AppCompatActivity {
+
+    int seconds = 0;
+    boolean running = false;
+    boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+
+        run_timer();
     }
 
     public void start_timer(View view) {
+        running = true;
     }
 
     public void stop_timer(View view) {
-
+        running = false;
     }
 
     public void restart_timer(View view) {
+        running = false;
+        seconds = 0;
+    }
 
+    private void run_timer() {
+        final TextView timeView = findViewById(R.id.timer);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                @SuppressLint("DefaultLocale")
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if(running) seconds++;
+
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(wasRunning) running = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
     }
 }
